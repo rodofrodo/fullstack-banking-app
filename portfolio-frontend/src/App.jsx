fa32f4f366963baf26d59a5a667449019325d03d
rodofrodo <rodofrodo708@gmail.com>
2026-07-08 12:41:22 +0200

    Modified App.jsx

--- portfolio-frontend/src/App.jsx
+++ portfolio-frontend/src/App.jsx
@@ -2,35 +2,45 @@ import { useState } from 'react';
 import './App.css';
 import Login from './Login';
 import Dashboard from './Dashboard';
+import Home from './Home';
+import Register from './Register';
 
 function App() {
     // checking if we have a token
     const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem('jwt_token'));
+  	const [currentView, setCurrentView] = useState('home'); // also: 'login', 'register'
 
     const handleLogout = () => {
 		localStorage.removeItem('jwt_token'); // getting rid of the token
 		setIsLoggedIn(false); // changing the state to logged-out
+		setCurrentView('home'); // returning to the home view
     };
 
-    return (
-        <div style={{ textAlign: 'center', fontFamily: 'sans-serif', padding: '20px' }}>
-			<h1>🏦 Ancient Bank</h1>
-			
-			{/* view logic */}
-			{!isLoggedIn ? (
-				// if not logged in, show signing-in view
-				<Login onLoginSuccess={() => setIsLoggedIn(true)} />
-			) : (
-				// if logged in, show the dashboard and a button to log out
-				<div>
-					<button onClick={handleLogout} style={{ marginBottom: '20px', padding: '8px', cursor: 'pointer' }}>
-						Log Out
-					</button>
-					<Dashboard />
-				</div>
-			)}
+    if (isLoggedIn) {
+		return (
+		<div style={{ textAlign: 'center', fontFamily: 'sans-serif', padding: '20px' }}>
+			<h1>🏛️ Ancient Bank</h1>
+			<button onClick={handleLogout} style={{ marginBottom: '20px', padding: '8px', cursor: 'pointer' }}>
+				Log Out
+			</button>
+			<Dashboard />
 		</div>
-    )
+		);
+	}
+
+	// else: home, login, register
+	return (
+		<div>
+			{currentView === 'home' && <Home navigateTo={setCurrentView} />}	
+			{currentView === 'login' && (
+				<Login 
+				onLoginSuccess={() => setIsLoggedIn(true)} 
+				navigateTo={setCurrentView} // that allows Login to view home page
+				/>
+			)}
+			{currentView === 'register' && <Register navigateTo={setCurrentView} />}
+		</div>
+	);
 }
 
 export default App

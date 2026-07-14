72cff1ae23caba5c38d9913f3103e55a6ffb5953
rodofrodo <rodofrodo708@gmail.com>
2026-07-14 18:44:48 +0200

    Added Optional<AppUser> for the repository

--- portfolio-backend/portfolio-backend/src/main/java/pl/straczek/portfolio_backend/repository/AppUserRepository.java
+++ portfolio-backend/portfolio-backend/src/main/java/pl/straczek/portfolio_backend/repository/AppUserRepository.java
@@ -4,9 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.straczek.portfolio_backend.model.AppUser;
 
+import java.util.Optional;
+
 @Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Long>
 {
     // for Spring Boot
     boolean existsByEmail(String email);
+    // this will allow to filter the database faster
+    Optional<AppUser> findByEmail(String email);
 }

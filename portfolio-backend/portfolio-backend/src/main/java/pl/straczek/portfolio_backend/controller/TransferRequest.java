67db3fadc8a408d050d9146f00b3f0c5de0dcbc6
rodofrodo <rodofrodo708@gmail.com>
2026-07-20 12:43:27 +0200

    Added currency to a transfer request

--- portfolio-backend/portfolio-backend/src/main/java/pl/straczek/portfolio_backend/controller/TransferRequest.java
+++ portfolio-backend/portfolio-backend/src/main/java/pl/straczek/portfolio_backend/controller/TransferRequest.java
@@ -2,6 +2,9 @@ package pl.straczek.portfolio_backend.controller;
 
 import java.math.BigDecimal;
 
-public record TransferRequest(String fromAccountNumber, String toAccountNumber, BigDecimal amount)
+public record TransferRequest(String fromAccountNumber,
+                              String toAccountNumber,
+                              BigDecimal amount,
+                              String currency)
 {
 }

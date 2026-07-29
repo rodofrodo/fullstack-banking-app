0a38653c58de20491464b79dd28182a5a0d1ed5a
rodofrodo <rodofrodo708@gmail.com>
2026-07-29 15:15:06 +0200

    New file for global methods

---
+++ portfolio-frontend/src/global/utils.js
@@ -0,0 +1,20 @@
+// global utils
+
+export const formatAccountNumber = (val) => {
+    if (!val) return '';
+
+    // we remove all whitespaces
+    const clean = val.replace(/\s/g, '').toUpperCase();
+
+    // if not starts with PL, we continue typing in
+    if (!clean.startsWith('PL')) return clean;
+
+    // the algorithm
+    const parts = [];
+    parts.push(clean.substring(0, 2)); // PL
+    if (clean.length > 2) parts.push(clean.substring(2, 4));
+    for (let i = 4; i < clean.length; i += 4)
+        parts.push(clean.substring(i, i + 4));
+
+    return parts.join(' ').substring(0, 35);
+};

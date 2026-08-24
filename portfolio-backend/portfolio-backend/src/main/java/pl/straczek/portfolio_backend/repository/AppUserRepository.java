f078bd2c362e3e831c7716bc30acbe3e981fc143
rodofrodo <rodofrodo708@gmail.com>
2026-08-24 14:25:35 +0200

    Added new query to AppUserRepository

--- portfolio-backend/portfolio-backend/src/main/java/pl/straczek/portfolio_backend/repository/AppUserRepository.java
+++ portfolio-backend/portfolio-backend/src/main/java/pl/straczek/portfolio_backend/repository/AppUserRepository.java
@@ -1,9 +1,12 @@
 package pl.straczek.portfolio_backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.straczek.portfolio_backend.model.AppUser;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -13,4 +16,8 @@ public interface AppUserRepository extends JpaRepository<AppUser, Long>
     boolean existsByEmail(String email);
     // this will allow to filter the database faster
     Optional<AppUser> findByEmail(String email);
+
+    @Query("SELECT DISTINCT b.owner FROM BankAccount b WHERE b.paymentCard IS " +
+            "NOT NULL AND LOWER(b.owner.username) LIKE LOWER(CONCAT('%', :query, '%'))")
+    List<AppUser> searchUsersWithCards(@Param("query") String query);
 }

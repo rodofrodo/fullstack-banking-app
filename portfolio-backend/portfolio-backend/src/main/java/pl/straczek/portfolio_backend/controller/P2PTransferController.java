fdf1e488a23e92edc300e30f36bd5b168ce8f0c5
rodofrodo <rodofrodo708@gmail.com>
2026-08-27 19:02:14 +0200

    Added new P2P transfer mechanics

--- portfolio-backend/portfolio-backend/src/main/java/pl/straczek/portfolio_backend/controller/P2PTransferController.java
+++ portfolio-backend/portfolio-backend/src/main/java/pl/straczek/portfolio_backend/controller/P2PTransferController.java
@@ -1,10 +1,15 @@
 package pl.straczek.portfolio_backend.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pl.straczek.portfolio_backend.dto.P2PTransferRequest;
 import pl.straczek.portfolio_backend.dto.UserSearchResult;
 import pl.straczek.portfolio_backend.model.AppUser;
+import pl.straczek.portfolio_backend.model.BankAccount;
 import pl.straczek.portfolio_backend.repository.AppUserRepository;
+import pl.straczek.portfolio_backend.repository.BankAccountRepository;
+import pl.straczek.portfolio_backend.repository.WalletRepository;
 
 import java.security.Principal;
 import java.util.List;
@@ -15,11 +20,17 @@ import java.util.List;
 public class P2PTransferController
 {
     private final AppUserRepository userRepository;
+    private final BankAccountRepository bankAccountRepository;
+    private final WalletRepository walletRepository;
 
     // ctor
-    public P2PTransferController(AppUserRepository userRepository)
+    public P2PTransferController(AppUserRepository userRepository,
+                                 BankAccountRepository bankAccountRepository,
+                                 WalletRepository walletRepository)
     {
         this.userRepository = userRepository;
+        this.bankAccountRepository = bankAccountRepository;
+        this.walletRepository = walletRepository;
     }
 
     @GetMapping("/search")
@@ -40,5 +51,63 @@ public class P2PTransferController
                 .toList();
 
         return ResponseEntity.ok(results);
+    }
+
+    @PostMapping("/transfer")
+    public ResponseEntity<?> executeP2PTransfer(@RequestBody P2PTransferRequest request, Principal principal)
+    {
+        // we check the sender and their account
+        AppUser sender = userRepository.findByEmail(principal.getName()).orElse(null);
+        BankAccount senderAccount = bankAccountRepository
+                .findByAccountNumber(request.fromAccountNumber()).orElse(null);
+
+        if (senderAccount == null || !senderAccount.getOwner().equals(sender))
+            return ResponseEntity
+                    .status(HttpStatus.FORBIDDEN)
+                    .body("This is not your account!");
+
+        if (senderAccount.getPaymentCard() == null)
+            return ResponseEntity.badRequest().body("This account doesn't have any connected card!");
+
+        // searching for the first proper wallet
+        var senderWallet = senderAccount.getWallets().stream()
+                .filter(w -> w.getCurrency().equals(request.currency()))
+                .findFirst()
+                .orElse(null);
+
+        if (senderWallet == null || senderWallet.getBalance().compareTo(request.amount()) < 0)
+            return ResponseEntity.badRequest().body("Insufficient funds in " + request.currency() + " wallet.");
+
+        // we're looking for the receiver
+        AppUser receiver = userRepository.findByUsername(request.targetUsername()).orElse(null);
+        if (receiver == null)
+            return ResponseEntity.badRequest().body("User not found.");
+
+        BankAccount receiverAccount = bankAccountRepository.findByOwner(receiver).stream()
+                .filter(account -> account.getPaymentCard() != null)
+                .findFirst()
+                .orElse(null);
+
+        if (receiverAccount == null)
+            return ResponseEntity.badRequest().body("he receiver does not have a card connected for P2P transfers.");
+
+        // searching for the receiver's wallet of the same currency
+        var receiverWallet = receiverAccount.getWallets().stream()
+                .filter(w -> w.getCurrency().equals(request.currency()))
+                .findFirst()
+                .orElse(null);
+
+        if (receiverWallet == null) {
+            // TODO: create a new wallet for the receiver
+            return ResponseEntity.badRequest().body("The receiver doesn't have a " + request.currency() + " wallet to receive funds.");
+        }
+
+        // MONEY TRANSFER
+        senderWallet.setBalance(senderWallet.getBalance().subtract(request.amount()));
+        receiverWallet.setBalance(receiverWallet.getBalance().add(request.amount()));
+        walletRepository.save(senderWallet);
+        walletRepository.save(receiverWallet);
+
+        return ResponseEntity.ok("P2P transfer to " + receiver.getUsername() + " successful!");
     }
 }
